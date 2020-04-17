c21e06ba348a0374caa226a5af1c2bbb311da503
Gaurav Kumar Mathur <mathuranant123@gmail.com>
2020-04-17 13:09:07 +0530

    Updated Permutation file

--- Assignment-2/Permutation.java
+++ Assignment-2/Permutation.java
@@ -1,16 +1,3 @@
-/*********************************************************************************************************************
- *  Author                 : Deepak Kumar Sood
- *  Date                   : 7-Jan-2017
- *  Last updated           : 7-Jan-2017
- * 
- *  Compilation            : use DrJava
- *  Execution              : java-algs4 Permutation 3 < distinct.txt
- *
- *  Purpose of the program : Client program for using RandomizedQueue
- *  Assingment link        : http://coursera.cs.princeton.edu/algs4/assignments/queues.html
- *  Score                  : 100/100
-**********************************************************************************************************************/
-
 import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
